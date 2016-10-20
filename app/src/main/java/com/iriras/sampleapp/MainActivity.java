5f6f55582280cb954ada185f59aa02725a925846
Irfan Irawan Sukirman <irfan.irawan@gits.co.id>
2016-10-20 13:13:05 +0700

    Commit

--- app/src/main/java/com/iriras/sampleapp/MainActivity.java
+++ app/src/main/java/com/iriras/sampleapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.iriras.sampleapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
